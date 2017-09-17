9fa9abf1606413d91984c5c5778b1dd6adffd0d3
Lukas Westermann <lukas.westermann@adnovum.ch>
2017-09-17 13:49:17 +0200

    fixed editor specs

--- spec/change_proxy/editor_spec.rb
+++ spec/change_proxy/editor_spec.rb
@@ -3,17 +3,42 @@ require 'change_proxy/env'
 require 'change_proxy/editor'
 
 RSpec.describe ChangeProxy::Editor do
-	subject { described_class.new('spec/fixtures/gradle.properties', %w{http}) }
+	let(:klass) do
+		Class.new do
+			attr_reader :file, :items
+
+			def self.load(file); new(file) end
+			def initialize(file)
+				@file = file
+				@items = []
+			end
+
+			def changed?
+				items.length > 0
+			end
+
+			def add(proto, proxy, no_proxy)
+				items << [proto.to_sym, proxy.to_s, no_proxy]
+			end
+		end
+	end
+
+	subject { described_class.new(klass, 'spec/fixtures/gradle.properties', %w{http}) }
 
 	context '#initialize' do
+		it 'raises ArgumentError when klass does not implement .load' do
+			expect { described_class.new(nil, 'spec/fixtures/gradle.properties') }.to raise_error(ArgumentError)
+			expect { described_class.new(Object, 'spec/fixtures/gradle.properties') }.to raise_error(ArgumentError)
+		end
+
 		it 'raises a Thor::Error when the file does not exist' do
-			expect { described_class.new('invalid/file') }.to raise_error(Thor::Error, /file not found/)
-			expect { described_class.new(nil) }.to raise_error(Thor::Error, /file not found/)
+			expect { described_class.new(klass, 'invalid/file') }.to raise_error(Thor::Error, /file not found/)
+			expect { described_class.new(klass, nil) }.to raise_error(Thor::Error, /file not found/)
 		end
 
 		it 'raises a Thor::Error when there are no protocols' do
-			expect { described_class.new('spec/fixtures/gradle.properties', []) }.to raise_error(Thor::Error, /No protocols/)
-			expect { described_class.new('spec/fixtures/gradle.properties', nil) }.to raise_error(Thor::Error, /No protocols/)
+			expect { described_class.new(klass, 'spec/fixtures/gradle.properties', []) }.to raise_error(Thor::Error, /No protocols/)
+			expect { described_class.new(klass, 'spec/fixtures/gradle.properties', nil) }.to raise_error(Thor::Error, /No protocols/)
 		end
 	end
 
@@ -21,8 +46,8 @@ RSpec.describe ChangeProxy::Editor do
 		let(:env) { ChangeProxy::Env.new('proxy' => 'cache:1080', 'http_proxy' => 'cache:3128', 'no_proxy' => 'example.org,example.net,example.com') }
 		let(:props) { subject.rewrite(env) }
 
-		it 'returns a ChangeProxy::Gradle::Props instance' do
-			expect(props).to be_a ChangeProxy::Gradle::Props
+		it 'returns a <klass> instance' do
+			expect(props).to be_a klass
 		end
 
 		it 'has changes' do
@@ -30,19 +55,8 @@ RSpec.describe ChangeProxy::Editor do
 		end
 
 		it 'has the new contents' do
-			expect(props.lines).to include("systemProp.http.proxyHost=cache\n", "systemProp.http.proxyPort=3128\n")
-		end
-
-		context 'with gradle-nochanges.properties' do
-			subject { described_class.new('spec/fixtures/gradle-nochanges.properties', %w{http}) }
-
-			it 'has no changes' do
-				expect(props.changed?).to be_falsey
-			end
-
-			it 'yet it still has the contents' do
-				expect(props.lines).to include("systemProp.http.proxyHost=cache\n", "systemProp.http.proxyPort=3128\n")
-			end
+			expect(props.items.length).to eq 1
+			expect(props.items.first).to eq [:http, "cache:3128", %w{example.org example.net example.com}]
 		end
 	end
 end
